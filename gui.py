bfbba790ebc964666e5fb859712d79ceb017c42b
Unknown <auratus.lemma@gmail.com>
2017-06-02 05:57:59 +0900

    Set custom config path to SAYA_CONFIG_PATH.

--- gui.py
+++ gui.py
@@ -14,14 +14,15 @@ import define as DEFINE
 _CURRENTPATH = os.path.dirname(os.path.realpath(__file__))
 
 
-if os.environ.get('SAYA_CONFIG'):
-    f = open(os.environ.get('SAYA_CONFIG'), 'r')
+if os.environ.get('SAYA_CONFIG_PATH'):
+    config_file = os.path.join(os.environ.get('SAYA_CONFIG_PATH'), 'saya.yaml')
+    f = open(config_file, 'r')
     CONFIG = yaml.load(f)
 else:
     f = open(os.path.join(_CURRENTPATH, 'config', 'saya.yaml'), 'r')
     CONFIG = yaml.load(f)
-    print CONFIG
 
+print CONFIG
 
 
 class MainWindow(QtGui.QMainWindow):
