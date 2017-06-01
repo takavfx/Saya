94e9d314fa27e41f2d95f3f0600e2eaeddbdd873
Unknown <auratus.lemma@gmail.com>
2017-06-02 07:19:46 +0900

    Add load config script.

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+_CURRENTPATH = os.path.dirname(os.path.realpath(__file__))
+
+
+def getConfig():
+    if os.environ.get('SAYA_CONFIG_PATH'):
+        config_file = os.path.join(os.environ.get('SAYA_CONFIG_PATH'), 'saya.yaml')
+        f = open(config_file, 'r')
+        CONFIG = yaml.load(f)
+    else:
+        f = open(os.path.join(_CURRENTPATH, 'config', 'saya.yaml'), 'r')
+        CONFIG = yaml.load(f)
+
+    print CONFIG
+    return CONFIG
