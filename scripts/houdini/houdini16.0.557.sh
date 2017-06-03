a07d0659960628b3981dc7f8e9516b62a292bfa2
Unknown <auratus.lemma@gmail.com>
2017-06-03 15:38:47 +0900

    Add sample script in scripts/houdini dir.

--- scripts/houdini/houdini16.0.557.sh
+++ scripts/houdini/houdini16.0.557.sh
@@ -1,7 +1,8 @@
-HOUDINI_VERSION="16.0.600"
+#!/bin/bash
+
+HOUDINI_VERSION="16.0.557"
 
 cd /opt/hfs$HOUDINI_VERSION
 source houdini_setup
-cd $home
 
 houdini
---
+++ scripts/houdini/houdini16.0.600.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+HOUDINI_VERSION="16.0.600"
+
+cd /opt/hfs$HOUDINI_VERSION
+source houdini_setup
+cd $home
+
+houdini
