3c0d43d992af36c0386c29b59f67d0452d53d24d
Unknown <auratus.lemma@gmail.com>
2017-06-07 02:11:44 +0900

    Manage code line.

--- gui.py
+++ gui.py
@@ -150,11 +150,6 @@ class MainWindow(QtCore.QObject):
 
 
 
-
-
-
-
-
 def main():
     import sys
 
@@ -162,6 +157,7 @@ def main():
     mainWindow = MainWindow()
     mainWindow.show()
     sys.exit(app.exec_())
+
 
 
 if __name__ == '__main__':
