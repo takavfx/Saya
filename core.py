2578545612d38b534b785fd321edab1ae03f8167
Unknown <auratus.lemma@gmail.com>
2017-06-10 12:05:47 +0900

    Remove multiprocessing module.

--- core.py
+++ core.py
@@ -1,15 +1,9 @@
 import os
 import subprocess
-from multiprocessing import Process
 
 _CURRENTPATH = os.path.dirname(os.path.realpath(__file__))
 
 
-def call(cmds):
+def launch(cmds):
     proc = subprocess.Popen(cmds)
     proc.communicate()
-
-def launch(cmds):
-    p = Process(target=call, args=(cmds,))
-    p.start()
-    p.join()
